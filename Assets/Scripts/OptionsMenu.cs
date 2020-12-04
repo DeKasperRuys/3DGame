4136e3673fd64790fddeba92d2fcd25ca674aee3
Kasper <kasper.ruys@student.ap.be>
2020-12-04 11:24:26 +0100

    PostProcessingTesting
    These are tests for the VR experience in Immersive Storytelling

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -7,6 +7,9 @@ public class OptionsMenu : MonoBehaviour
 {
 
     public GameObject menuMain;
+    public GameObject Level1, Level2;
+    private bool isLevel1Selected = false;
+    private bool isLevel2Selected = false;
     private void Start()
     {
         Cursor.visible = true;
@@ -20,10 +23,29 @@ public class OptionsMenu : MonoBehaviour
 
     public void SelectLevel1()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        isLevel1Selected = true;
+        isLevel2Selected = false;
+        Level1.SetActive(true);
+        Level2.SetActive(false);
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void SelectLevel2()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        isLevel1Selected = false;
+        isLevel2Selected = true;
+        Level2.SetActive(true);
+        Level1.SetActive(false);
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void StartLevel()
+    {
+        if (isLevel1Selected)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if (isLevel2Selected)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
     }
 }
